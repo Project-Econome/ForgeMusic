92d8d61c347723ea5d5b6904def45679b7c031db
Project Econome <108093514+Project-Econome@users.noreply.github.com>
2024-06-29 19:13:08 +0000

    updated libs and src to match latest

--- lib/natives/getSoundsFolder.d.ts
+++ lib/natives/getSoundsFolder.d.ts
@@ -1,3 +1,3 @@
-import { NativeFunction } from "forgescript";
+import { NativeFunction } from "@tryforge/forgescript";
 declare const _default: NativeFunction<import("forgescript").IArg<import("forgescript").ArgType, boolean, boolean, import("forgescript").EnumLike<any>>[], false>;
 export default _default;
